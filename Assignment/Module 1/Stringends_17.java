de0e6fe2cccc606d61f0e8fccf90d5b32165eb4b
shruti100925 <120240503+shruti100925@users.noreply.github.com>
2023-04-24 10:00:24 +0530

    Add files via upload

---
+++ Assignment/Module 1/Stringends_17.java
@@ -0,0 +1,15 @@
+package com.assignment;
+
+public class Stringends_17
+{
+	public static void main(String[] args) 
+	{
+		String str= "Java Exercises";
+		String str1= "Java Exercise";
+		
+		System.out.println(str.endsWith("se"));
+		System.out.println(str1.endsWith("se"));
+		
+	}
+
+}
