d18f2144c532c6fb9abd933d6bac84eb731dd318
shruti100925 <120240503+shruti100925@users.noreply.github.com>
2023-06-15 16:56:30 +0530

    Add files via upload

---
+++ Assignment/Module 4/5.Studentinfo_SQL/Model.java
@@ -0,0 +1,52 @@
+package com.model;
+
+public class Model
+{
+	int id;
+	String fname,lname,email,mobile,gender,pass;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+
+}
---
+++ Assignment/Module 4/5.Studentinfo_SQL/insertdata.jsp
@@ -0,0 +1,43 @@
+<%@page import="com.dao.Dao"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <jsp:useBean id="m" class="com.model.Model"/>
+	<jsp:setProperty property="*" name="m"/>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+<%
+	String name=request.getParameter("fname");
+    String fname=request.getParameter("lname");
+    String email=request.getParameter("email");
+    String mobile=request.getParameter("mobile");
+    String gender=request.getParameter("gender");
+    String pass=request.getParameter("pass");
+    
+    int status=Dao.savedata(m);
+    if(status>0)
+	{
+		//out.print("Success");
+		response.sendRedirect("viewdata.jsp");
+	}
+	else
+	{
+		out.print("Fail");
+	}
+    
+
+	
+	%>
+	
+
+
+
+
+</body>
+</html>
